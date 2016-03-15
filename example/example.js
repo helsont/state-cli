2e1022f5f088708efd6c0abde730f3b4073a222d
helsont <helsont@gmail.com>
2016-03-15 05:40:09 -0400

    Added goto state functionality. Added optional queries

--- example/example.js
+++ example/example.js
@@ -2,12 +2,29 @@ var FSM = require('../index');
 var CLI = require('../lib/cli');
 
 var States = new FSM();
+var main = {};
+var db = {
+  1: 'A',
+  2: 'B',
+  3: 'C',
+  4: 'D'
+};
+
+var myDb = {
+  1: 'X',
+  2: 'Y'
+};
+
+main.db = db;
+main.myDb = myDb;
 
 States.on([
   'see',
   'list',
   'buy',
   'sell',
+  'confirm',
+  'create',
   'help'
 ]);
 
@@ -16,43 +33,71 @@ States.on('see', [
     contractId: 'integer'
   },
   'buy',
-  {
-    contractId: 'string',
-    messengerId: 'integer'
-  },
   'sell'
-], function(q) {
-  console.log(q);
-  // load data
-  // return data
+], function(q, input) {
+  q.contractId = input.contractId;
+  console.log('You can purchase ' + input.contractId);
 });
 
 States.on('buy', {
-  shares: 'integer',
-  price: 'integer'
-}, function(q) {
-  console.log('buy', q.shares, q.price);
-})
+  id: 'integer'
+}, function(q, input) {
+  myDb[input.id].id = db[input.id];
+  delete db[input.id];
+});
 
-States.on('sell', {
-  shares: 'integer',
-  price: 'integer'
-}, function(q) {
+States.on('sell', [
+  {
+    id: 'integer'
+  },
+  'confirm'
+], function(q, input) {
   debugger;
-  console.log('sell', q.shares, q.price);
-})
+  q.goto('confirm', input.id);
+});
 
-States.on('help', function(q) {
-  console.log('Commands: ' + 'see, list, sell, buy.');
+States.on('confirm', function(q, id) {
+  console.log('Are you sure you want to purchase this?');
+
+  db[id] = myDb[id];
+  delete myDb[id];
+});
+
+States.on('help', function() {
+  console.log('Commands: ' + 'see, list, sell, create, buy.');
+});
+
+States.on('create', {
+  id: 'integer',
+  value: 'string'
+}, function(q, input) {
+  console.log(input);
+  myDb[input.id] = input.value;
+  debugger;
 });
 
 // auto parameter inference
-States.on('list', function(q) {
+States.on('list', [{
+  which: 'string?'
+}],
+function(q, input) {
+  var value = input.which || 'myDb';
+  var db = main[value];
+  if (!db) {
+    throw new Error(`Could not find ${value}, try 'myDb' or 'db'`);
+  }
 
-  // display the market shares
-  console.log('we\'ve arrived at the list');
+  var obj = Object.keys(main[value]);
+  for (var idx = 0, len = obj.length; idx < len; idx++) {
+    var res = main[value][obj[idx]];
+    console.log(obj[idx] + ' : ' + res);
+  }
+  console.log();
+
 });
 
 States.done();
 
 new CLI(States);
+// States.input('see 10');
+// States.input('sell 10 92');
